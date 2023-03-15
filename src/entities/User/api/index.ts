e44fa0ad8954260fb25f0901eaaa5691a993e450
YakubovKirillBicycle <kirill.yakubov@mbicycle.com>
2023-03-15 23:51:07 +0300

    mock user data

--- src/entities/User/api/index.ts
+++ src/entities/User/api/index.ts
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 import { ERROR } from 'shared/model/constants';
+import { ADMIN_USER, USERS } from 'shared/model/mockData';
 
 import { EMPTY_USER, Role, Status, User } from '../model/types';
 
@@ -12,6 +13,17 @@ export const fakeGetUser = (
     return fetch('https://pokeapi.co/api/v2/pokemon/ditto')
         .then((res) => {
             if (res.status === 404) throw new Error(ERROR[res.status]);
+
+            if (password === 'admin') return ADMIN_USER;
+            if (password === 'user') {
+                return USERS.find((user) => user.nickname === userName) || {
+                    ...EMPTY_USER,
+                    id: uuidv4(),
+                    nickname: userName,
+                    role: 'manager' as unknown as Role,
+                    status: 'online' as unknown as Status
+                }
+            }
 
             return ({
                 ...EMPTY_USER,
